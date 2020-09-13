06b556e57b2b82137f0d5e28de3e7e3a05f71f59
Roshara Gardiyawasam <rgar0002@student.monash.edu>
2020-09-13 21:34:59 +1000

    Update pong.ts

--- pong.ts
+++ pong.ts
@@ -4,8 +4,9 @@ import { map, filter, merge, scan} from 'rxjs/operators';
 const 
   Constants = new class {
     readonly CanvasSize = 600;
-    readonly Bu
-  }
+    readonly BallAcc = 0.1;
+    readonly ;
+  };
 
 type Key = 'ArrowUp' | 'ArrowDown' | 'Space'
 
@@ -18,8 +19,28 @@ type Entity = Readonly<{
 
 }>
 
-type State = {
-    
+type player1State = {
+
+}
+
+/**
+ * Simple Vector Math Class
+ * adapted from Tim Dwyer's Work @https://tgdwyer.github.io/asteroids/
+ */
+class Vector {
+  constructor(public readonly x: number = 0, public readonly y: number = 0) {}
+  add = (v:Vector) => new Vector(this.x + v.x, this.y + v.y)
+  sub = (v:Vector) => new Vector(this.x - v.x, this.y - v.y)
+  len = () => Math.sqrt(this.x**2 + this.y**2)
+  scale = (s:number) => new Vector(this.x*s, this.y*s)
+  ortho = ()=> new Vector(this.y, -this.x)
+  rotate = (deg:number) => 
+    (rad =>(
+      (cos,sin,{x,y})=>new Vector(x*cos - y*sin, x*sin + y*cos)
+     )(Math.cos(rad), Math.sin(rad), this)
+    )(Math.PI * deg / 180)
+  static unitVecInDirection = (deg: number) => new Vector(0,-1).rotate(deg)
+  static Zero = new Vector();
 }
 
 function pong() {
