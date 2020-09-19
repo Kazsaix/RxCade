467bb5783402527b7b449d1916f50da1d5573f88
Roshara Gardiyawasam <rgar0002@student.monash.edu>
2020-09-19 22:18:49 +1000

    finish game

--- pong.ts
+++ pong.ts
@@ -1,19 +1,61 @@
 import { fromEvent, interval, from, Observable } from 'rxjs';
-import { map, filter, merge, scan, flatMap, takeUntil} from 'rxjs/operators';
+import { map, filter, merge, scan, flatMap, takeUntil, take} from 'rxjs/operators';
+import {Elem} from './svgElementHelper';
 
-//Game Settings Constants
 const 
   gameSettings = new class {
     readonly CanvasSize = 600;
     readonly PaddleLength = 100;
     readonly PaddleWidth = 10;
     readonly PaddleOffset = 5;
-    readonly PaddleSpeed = 7;
-    readonly BallRadius = 10;
+    readonly PaddleOffset = 15;
+    readonly PaddleSpeed = 9;
+    readonly PaddleAiSpeed = 7;
+    readonly BallRadius = 5;
     readonly BallAcc = 0.1;
+    readonly BallMaxBounceAngle = 75;
     readonly WinningScore = 7;
     readonly InbetweenRoundInterval = 2;
   }
+
+
+const canvasSvg = document.getElementById("canvas")!,
+  playerOnePaddleSvg = new Elem(canvasSvg, "rect", "paddlePlayerOne" ),
+  playerTwoPaddleSvg = new Elem(canvasSvg, "rect", "paddlePlayerTwo"),
+  pongBallSvg = new Elem(canvasSvg, "rect", "pongBall"),
+  centreLineSvg = new Elem(canvasSvg, "line", "centreLine"),
+  courtTopLineSvg = new Elem(canvasSvg, "line", "courtTopLine"),
+  courtBottomLineSvg = new Elem(canvasSvg, "line", "courtBottomLine"),
+  playerOneScoreSvg = new Elem(canvasSvg, "text", "playerOneScoreText"),
+  playerTwoScoreSvg = new Elem(canvasSvg, "text", "playerTwoScoreText"),
+  gameOverTextSvg = new Elem(canvasSvg, "text", "gameOver"),
+  pauseGroupSvg = new Elem(canvasSvg, "g", "pauseIconGroup"),
+  pauseLeftRectSvg = new Elem(canvasSvg, "rect", "pauseLeftRectangle", pauseGroupSvg.elem),
+  pauseRightRectSvg = new Elem(canvasSvg, "rect", "pauseLeftRectangle", pauseGroupSvg.elem);
+
+playerOnePaddleSvg.mulAttr({width:gameSettings.PaddleWidth,height:gameSettings.PaddleLength,style:"fill:white",transform:`translate(${gameSettings.PaddleOffset}, ${gameSettings.CanvasSize/2-gameSettings.PaddleLength/2})`})
+playerTwoPaddleSvg.mulAttr({width:gameSettings.PaddleWidth,height:gameSettings.PaddleLength,style:"fill:white",transform:`translate(${gameSettings.CanvasSize-gameSettings.PaddleOffset}, ${gameSettings.CanvasSize/2-gameSettings.PaddleLength/2})`})
+pongBallSvg.mulAttr({width:10,height:10,style:"fill:white",transform:`translate(${gameSettings.CanvasSize/2-gameSettings.BallRadius/2}, ${gameSettings.CanvasSize/2-gameSettings.BallRadius/2})`})
+centreLineSvg.mulAttr({x1:gameSettings.CanvasSize/2,x2:gameSettings.CanvasSize/2,y1:0,y2:gameSettings.CanvasSize,style:"stroke:white", "stroke-width":3, "stroke-dasharray":"10, 8"})
+courtTopLineSvg.mulAttr({x1:"0",x2:gameSettings.CanvasSize,y1:"0",y2:"0",style:"stroke:white"})
+courtBottomLineSvg.mulAttr({x1:"0",x2:gameSettings.CanvasSize,y1:gameSettings.CanvasSize,y2:gameSettings.CanvasSize,style:"stroke:white"})
+playerOneScoreSvg.mulAttr({fill:"white","font-size":"4em",transform:`translate(${gameSettings.CanvasSize/2-gameSettings.CanvasSize/3}, ${gameSettings.CanvasSize/6})`})
+playerOneScoreSvg.setTextContent("0")
+playerTwoScoreSvg.mulAttr({fill:"white","font-size":"4em",transform:`translate(${gameSettings.CanvasSize/2+gameSettings.CanvasSize/3}, ${gameSettings.CanvasSize/6})`})
+playerTwoScoreSvg.setTextContent("0")
+gameOverTextSvg.mulAttr({fill:"white","font-size":"3em",transform:`translate(0, ${gameSettings.CanvasSize/2})`})
+gameOverTextSvg.setTextContent("Game Over! Player X Won!")
+gameOverTextSvg.hideElement(true)
+pauseGroupSvg.hideElement(true)
+pauseLeftRectSvg.mulAttr({width:20,height:80,style:"fill:white",transform:`translate(${gameSettings.CanvasSize/2-40}, ${gameSettings.CanvasSize/2-40})`})
+pauseRightRectSvg.mulAttr({width:20,height:80,style:"fill:white",transform:`translate(${gameSettings.CanvasSize/2+20}, ${gameSettings.CanvasSize/2-40})`})
+
+
+//A very bad pure psuedorandom 
+const RNG = (seed:number):number => ((1103515245 * seed + 12345) % 0x80000000) / (0x80000000-1)
+
+const randomFloatGenerator = interval(10)
+const rngGenerator = RNG(99)
 
 type Key = 'ArrowUp' | 'ArrowDown' | 'KeyP' | 'KeyR'
 
@@ -24,6 +66,7 @@ type ViewType = 'ball' | 'paddle'
 type PlayState = 'Play' | 'Pause' | 'GameOver'
 
 type Player = 'PlayerOne' | 'PlayerTwo'
+
 type Entity = Readonly<{
   id: string,
   viewType: ViewType,
@@ -32,11 +75,14 @@ type Entity = Readonly<{
   acc:Vector,  
 }>
 
+type Paddle = Entity; 
+type Ball = Entity;
+
 type GameState = Readonly<{
   time:number,
-  playerOnePaddle:Entity,
-  playerTwoPaddle:Entity,
-  ballState:Entity,
+  playerOnePaddle:Paddle,
+  playerTwoPaddle:Paddle,
+  ballState:Ball,
   playState:PlayState,
   playerOneScore:number,
   playerTwoScore:number,
@@ -52,7 +98,7 @@ class Vector {
   sub = (v:Vector) => new Vector(this.x - v.x, this.y - v.y)
   len = () => Math.sqrt(this.x**2 + this.y**2)
   scale = (s:number) => new Vector(this.x*s, this.y*s)
-  scale2d = (s:number) => (t:number) => new Vector(this.x*s, this.y*t)
+  scaleXY = (s:number) => (t:number) => new Vector(this.x*s, this.y*t)
   ortho = ()=> new Vector(this.y, -this.x)
   rotate = (deg:number) => 
       (rad =>(
@@ -66,6 +112,7 @@ class Vector {
 class Tick { constructor(public readonly elapsed:number){}}
 class MovementDirection { constructor(public readonly direction: 'Up' | 'Down' | 'UpStop' | 'DownStop'){}}
 class Pause {constructor(){}}
+class Restart {constructor() {}}
 
 const createEntity = (id_string:string) => (view_type:ViewType) => (pos_vector:Vector) => <Entity>{
     id: id_string,
@@ -75,15 +122,17 @@ const createEntity = (id_string:string) => (view_type:ViewType) => (pos_vector:V
     acc: Vector.Zero
 }
 
-const randomIntBetween = (min:number) => (max:number) => Math.floor(Math.random()*(max-min+1)+min)
+const randomIntBetween = (seed:number) => (min:number) => (max:number) => Math.floor(RNG(seed)*(max-min+1)+min)
 
 const serveBall = (e:Entity) => (p:Player) => 
     p==='PlayerOne' ? {...e,
       pos: new Vector(gameSettings.CanvasSize/2, gameSettings.CanvasSize/2),
-      vel: Vector.unitVecInDirection(randomIntBetween(210)(330)).scale(randomIntBetween(3)(8))
+      vel: Vector.unitVecInDirection(randomIntBetween(210)(330)(Math.random())).scale(randomIntBetween(3)(8)(Math.random())),
+      acc: Vector.Zero
   } : {...e,
     pos: new Vector(gameSettings.CanvasSize/2, gameSettings.CanvasSize/2),
-    vel: Vector.unitVecInDirection(randomIntBetween(30)(150)).scale(randomIntBetween(3)(8))
+    vel: Vector.unitVecInDirection(randomIntBetween(30)(150)(Math.random())).scale(randomIntBetween(3)(8)(Math.random())), 
+    acc: Vector.Zero
 }
 
 const initialState:GameState = {
@@ -96,18 +145,15 @@ const initialState:GameState = {
   playerTwoScore: 0,
 }
 
-const moveEntity = (e:Entity) => e.viewType === 'paddle' ? <Entity>{
+const moveEntity = (e:Entity) =>   e.viewType === 'paddle'   ? <Entity>{
   ...e,
-  // pos: paddleCheckBounds(e.pos.add(e.vel)),
-  pos: new Vector(gameSettings.PaddleOffset, paddleCheckBounds(e.pos.add(e.vel).y)),
+  pos: new Vector(e.pos.x, entityCheckBounds(e.pos.add(e.vel).y)(gameSettings.PaddleLength)),
   vel: e.vel.add(e.acc),
 } : <Entity>{
   ...e,
-  pos: e.pos.add(e.vel),
+  pos: new Vector(entityCheckBounds(e.pos.x+e.vel.x)(gameSettings.BallRadius), entityCheckBounds(e.pos.y+e.vel.y)(gameSettings.BallRadius)),
   vel: e.vel.add(e.acc),
 }
-
-
 
 /**
  * A small function to check if two values a and b are within c of each other
@@ -118,50 +164,85 @@ const moveEntity = (e:Entity) => e.viewType === 'paddle' ? <Entity>{
 const near = (a:number) => (c:number) => (b:number):Boolean =>  Math.abs(a-b) <= c;
 
 // const paddleCheckBounds = (pos:Vector):Vector => pos.y < 0 ? new Vector(gameSettings.PaddleOffset, 0) : pos.y + gameSettings.PaddleLength > gameSettings.CanvasSize ? new Vector(gameSettings.PaddleOffset,gameSettings.CanvasSize-gameSettings.PaddleLength) : new Vector(gameSettings.PaddleOffset,pos.y);
-   const paddleCheckBounds = (pos:number):number => pos < 0 ?  0 : pos + gameSettings.PaddleLength > gameSettings.CanvasSize ? gameSettings.CanvasSize-gameSettings.PaddleLength : pos;
+const entityCheckBounds = (pos:number) => (entitySize:number):number => pos < 0 ?  0 : pos + entitySize > gameSettings.CanvasSize ? gameSettings.CanvasSize-entitySize : pos;
+const handleCollisions = (s:GameState) => paddleCollions(courtCollisions(s));
 
-const handleCollisions = (s:GameState) => courtCollisions(s);
-// const handleCollisions = (s:GameState) => s;
 
 const courtCollisions = (s:GameState) => 
     near(s.ballState.pos.y)(gameSettings.BallRadius)(0) || near(s.ballState.pos.y)(gameSettings.BallRadius)(gameSettings.CanvasSize) ?  {...s, 
         ballState: {...s.ballState,
-          vel: s.ballState.vel.scale2d(1)(-1)  
+          vel: s.ballState.vel.scaleXY(1)(-1)  
         }
       } : near(s.ballState.pos.x)(gameSettings.BallRadius)(0) ? {...s,
         ballState: serveBall(s.ballState)('PlayerOne'),
-        playerTwoScore: s.playerTwoScore + 1
+        playerTwoScore: s.playerTwoScore + 1,
+        playState: s.playerTwoScore + 1 >= gameSettings.WinningScore  ? 'GameOver'  as PlayState : s.playState
       }
       : near(s.ballState.pos.x)(gameSettings.BallRadius)(gameSettings.CanvasSize) ? {...s,
         ballState: serveBall(s.ballState)('PlayerTwo'),
-        playerOneScore: s.playerOneScore + 1
+        playerOneScore: s.playerOneScore + 1,
+        playState: s.playerOneScore + 1 >= gameSettings.WinningScore ? 'GameOver' as PlayState : s.playState
       } 
       : s
 
+  const paddleChecker = (paddle:Entity) => (ball:Entity) => paddle.pos.x < ball.pos.x + gameSettings.BallRadius &&
+      paddle.pos.y < ball.pos.y + gameSettings.BallRadius &&
+      ball.pos.x < paddle.pos.x + gameSettings.PaddleWidth &&
+      ball.pos.y < paddle.pos.y + gameSettings.PaddleLength
 
-// const paddleCollions = ()
+const paddleCollions = (s:GameState) => !ballTowardsAi(s.ballState) && paddleChecker(s.playerOnePaddle)(s.ballState) ? {...s,
+    ballState: {...s.ballState,
+      vel: ballBounceVelocity(s.playerOnePaddle)(s.ballState).scale(1.3),
+      // acc: ballBounceVelocity(s.playerOnePaddle)(s.ballState).scale(0.01)
+    }
+  } : ballTowardsAi(s.ballState) && paddleChecker(s.playerTwoPaddle)(s.ballState) ? {...s,
+        ballState: {...s.ballState,
+          vel: ballBounceVelocity(s.playerTwoPaddle)(s.ballState).scale(1.1),
+          acc: Vector.Zero
+       }
+      } : s
+
+
+const velVecInDirection = (direction:number) => (speed:number) => Vector.unitVecInDirection(direction).scale(speed)    
+const paddleOnAiSide = (ball:Entity) => ball.pos.x > gameSettings.CanvasSize/2 
+const ballTowardsAi = (ball:Entity) => ball.vel.x > 0
+const paddleAi = (paddle:Entity) => (ball:Entity):Entity => paddleOnAiSide(ball) && ballTowardsAi(ball) ? paddle.pos.y+gameSettings.PaddleLength/2 > ball.pos.y ? {...paddle, vel:velVecInDirection(0)(gameSettings.PaddleAiSpeed)} : {...paddle, vel:velVecInDirection(180)(gameSettings.PaddleAiSpeed)} : {...paddle, vel:Vector.Zero}
+
+const ballBounceVelocity = (paddle:Entity) => (ball:Entity):Vector => {
+  const rely = (paddle.pos.y+gameSettings.PaddleLength/2)-(ball.pos.y+gameSettings.BallRadius)
+  const bounceAngle = rely/(gameSettings.PaddleLength/2)*gameSettings.BallMaxBounceAngle 
+  // console.log(((paddle.pos.y+(gameSettings.PaddleLength/2))-(ball.pos.y+gameSettings.BallRadius))/(gameSettings.PaddleLength/2))
+  // console.log(paddle.pos.y+(gameSettings.PaddleLength/2))
+  // console.log((ball.pos.y+gameSettings.BallRadius))
+  // console.log(rely)
+  // console.log(bounceAngle)
+  return ballTowardsAi(ball) ? velVecInDirection(270+bounceAngle)(ball.vel.len()) : velVecInDirection(90-bounceAngle)(ball.vel.len())
+}
 
 const tick = (s:GameState, elapsed) => 
   s.playState === 'Play' ? handleCollisions({...s,
   playerOnePaddle: moveEntity(s.playerOnePaddle),
   ballState: moveEntity(s.ballState),
+  playerTwoPaddle: moveEntity(paddleAi(s.playerTwoPaddle)(s.ballState)),
   time: elapsed
-}) : s.playState === 'Pause' ? s : s
+// }) : s.playState === 'Pause' ? s : s //TODO gAMEOVER sTATE
+}) : s
 
 const reduceState = (s:GameState, e:MovementDirection|Pause|Tick) =>
   e instanceof MovementDirection ? {...s,
     playerOnePaddle : {...s.playerOnePaddle,
-      vel: e.direction === 'Up' ? Vector.unitVecInDirection(0).scale(gameSettings.PaddleSpeed) : 
-      e.direction === 'Down' ? Vector.unitVecInDirection(180).scale(gameSettings.PaddleSpeed) : 
-      e.direction === 'UpStop' ? s.playerOnePaddle.vel.sub(Vector.unitVecInDirection(0).scale(gameSettings.PaddleSpeed)) :    
-      s.playerOnePaddle.vel.sub(Vector.unitVecInDirection(180).scale(gameSettings.PaddleSpeed))
+      vel: e.direction === 'Up' ? velVecInDirection(0)(gameSettings.PaddleSpeed) : 
+      e.direction === 'Down' ? velVecInDirection(180)(gameSettings.PaddleSpeed) : 
+      e.direction === 'UpStop' ? s.playerOnePaddle.vel.sub(velVecInDirection(0)(gameSettings.PaddleSpeed)) :    
+      s.playerOnePaddle.vel.sub(velVecInDirection(180)(gameSettings.PaddleSpeed))
     }
   } : e instanceof Pause ? {...s,
-    playState: s.playState === 'Play' ? 'Pause' : 'Play'
-  } : tick(s, e.elapsed);
+    playState: s.playState === 'Play' ? 'Pause' as PlayState : s.playState === 'Pause' ? 'Play' as PlayState : 'GameOver' as PlayState 
+  } : e instanceof Restart ? restartGame(s)  
+  : tick(s, e.elapsed);
 
+const restartGame = (s:GameState) => s.playState == 'GameOver' ? initialState : s
 
-function pong() {
   const keyObservable = <T>(e:Event, k:Key, result:()=>T)=>
       fromEvent<KeyboardEvent>(document, e)
           .pipe(
@@ -173,37 +254,40 @@ function pong() {
       downMoveKeyDown = keyObservable('keydown', 'ArrowDown', ()=>new MovementDirection('Down')),
       upMoveKeyUp = keyObservable('keyup', 'ArrowUp', ()=> new MovementDirection('UpStop')),
       downMoveKeyUp = keyObservable('keyup', 'ArrowDown', ()=> new MovementDirection('DownStop')),
-      pauseKeyPress = keyObservable('keydown', 'KeyP', ()=> new Pause());
-    
-  const up = upMoveKeyDown;
-  up.subscribe(s=>console.log('UpPressed')) 
-      
-  const gameLoopObs = interval(10).pipe(
+      pauseKeyPress = keyObservable('keydown', 'KeyP', ()=> new Pause()),
+      restartKeyPress = keyObservable('keydown', 'KeyR', ()=>new Restart());
+                    
+  const pongGameObs =  interval(10).pipe(
     map(elapsed=>new Tick(elapsed)),
-    merge(upMoveKeyUp, downMoveKeyUp, upMoveKeyDown, downMoveKeyDown,  pauseKeyPress),
+    merge(upMoveKeyUp, downMoveKeyUp, upMoveKeyDown, downMoveKeyDown,  pauseKeyPress, restartKeyPress),
     scan(reduceState, initialState)
   )
-  
-  const g1 = gameLoopObs.subscribe(updateView),
-    g2 = gameLoopObs.pipe(
-      filter(s=> s.time % 1000 == 0)
-    ).subscribe(s=>console.log(s))
 
-    function updateView(s: GameState) {
-      const 
-        svg = document.getElementById("canvas"),
-        paddlePlayerOneSvg = document.getElementById("paddlePlayerOne"),
-        paddlePlayerTwoSvg = document.getElementById("paddlePlayerTwo"),
-        ballSvg = document.getElementById("pongBall"),
-        attr = (e:Element) => (o:any) => { for(const k in o) e.setAttribute( k, String(o[k]))};
-      attr(paddlePlayerOneSvg)({transform:`translate(${Math.round(s.playerOnePaddle.pos.x)} ${Math.round(s.playerOnePaddle.pos.y)})`});
-      attr(paddlePlayerTwoSvg)({transform:`translate(${Math.round(s.playerTwoPaddle.pos.x)} ${Math.round(s.playerTwoPaddle.pos.y)})`});
-      attr(ballSvg)({transform:`translate(${Math.round(s.ballState.pos.x)} ${Math.round(s.ballState.pos.y)})`});
-      if (s.playState == 'GameOver') {
-        g1.unsubscribe();
-        // const gameOverMessage
-      }
+
+  const startGame = () => pongGameObs.subscribe(updateView) 
+
+  function updateView(s: GameState) {
+    playerOnePaddleSvg.attr("transform",`translate(${Math.round(s.playerOnePaddle.pos.x)} ${Math.round(s.playerOnePaddle.pos.y)})`)
+    playerTwoPaddleSvg.attr("transform",`translate(${Math.round(s.playerTwoPaddle.pos.x)} ${Math.round(s.playerTwoPaddle.pos.y)})`)
+    pongBallSvg.attr("transform",`translate(${Math.round(s.ballState.pos.x)} ${Math.round(s.ballState.pos.y)})`)
+    playerOneScoreSvg.setTextContent(s.playerOneScore.toString())
+    playerTwoScoreSvg.setTextContent(s.playerTwoScore.toString())
+    if (s.playState == 'Pause') {
+      pauseGroupSvg.hideElement(false)
+    } else {
+      pauseGroupSvg.hideElement(true)
     }
+    if (s.playState == "GameOver") {
+      gameOverTextSvg.setTextContent(`Game Over! Player ${s.playerOneScore > s.playerTwoScore ? "One" : "Two"} Won!`)
+      gameOverTextSvg.hideElement(false)
+    } else {
+      gameOverTextSvg.hideElement(true)
+    }
+  }
+
+function pong() {
+    startGame();
+    pongGameObs.pipe(filter((s:GameState)=>s.time%1000===0)).subscribe(console.log)
   }
 
   // the following simply runs your pong function on window load.  Make sure to leave it in place.
--- svgElementHelper.ts
+++ svgElementHelper.ts
@@ -1,20 +1,29 @@
-
-
+export{Elem}
 class Elem {
     elem: Element;
 
-    constructor(svg: HTMLElement, id:string, parent: Element = svg) {
-        this.elem = document.createElementNS(svg.namespaceURI, id);
+    constructor(svgCanvas: HTMLElement, element_type:string, id: string, parent: Element = svgCanvas) {
+        this.elem = document.createElementNS(svgCanvas.namespaceURI, element_type);
+        this.attr("id",id)
         parent.appendChild(this.elem);
     }
 
-    attr(name: string): string
-    attr(name: string, value: string | number): this
-    attr(name: string, value?: string | number): this | string {
-        if (typeof value === 'undefined') {
-          return this.elem.getAttribute(name)!;
-        }
+    getAttr(name: string): string {
+      return this.elem.getAttribute(name)!;
+    }
+    attr(name: string, value: string | number): this {
         this.elem.setAttribute(name, value.toString());
         return this;
       }
+    mulAttr(object:any): this {
+      for(const k in object) this.attr( k, String(object[k]));
+      return this;
+    }
+    hideElement(condition:boolean) {
+      this.attr("visibility", condition ? "hidden" : "visible")
+    }
+
+    setTextContent(text:string) {
+      this.elem.textContent = text;
+    }
 }
